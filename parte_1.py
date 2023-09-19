a6c511e8b0cd31a025ccc28cc8609b60aeade960
Eidy Sipion <eidysipion17@gmail.com>
2023-09-19 17:35:52 -0500

    promedio de personas en estudio

---
+++ parte_1.py
@@ -0,0 +1,16 @@
+from datasets import load_dataset
+
+dataset = load_dataset("mstz/heart_failure")
+
+data = dataset["train"]
+
+import numpy as np
+
+edades = data["age"]
+
+edades_np = np.array(edades)
+
+promedio_edad = np.mean(edades_np)
+
+print("El promedio de edad de las personas participantes en el estudio es:", promedio_edad)
+
