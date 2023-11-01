c9e63b278eb71035d9beb697780962c557220f45
Eidysipion <137240117+Eidysipion@users.noreply.github.com>
2023-11-01 09:48:24 -0500

    Create parte_2.py

---
+++ parte_2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from datasets import load_dataset
+
+dataset = load_dataset("mstz/heart_failure")
+data = dataset["train"]
+
+
+df = pd.DataFrame(data)
+
+df_perecieron = df[df["is_dead"] == 1]
+df_no_perecieron = df[df["is_dead"] == 0]
+
+
+promedio_edad_perecieron = df_perecieron["age"].mean()
+promedio_edad_no_perecieron = df_no_perecieron["age"].mean()
+
+
+print("Promedio de edades de personas que perecieron:", promedio_edad_perecieron)
+print("Promedio de edades de personas que no perecieron:", promedio_edad_no_perecieron)
